c8fdb368fff72111abff115f699cb626d73b81c2
supersmau <samuel.lopez.asuncion@gmail.com>
2015-09-10 18:37:04 +0200

    AFE serial without interrupts

--- code/AFE/AFE_serial.c
+++ code/AFE/AFE_serial.c
@@ -7,7 +7,6 @@
 
 #include "AFE_serial.h"
 
-volatile uint8_t AFE_TX_data = 0x00;
 volatile uint8_t AFE_RX_data = 0x00;
 
 volatile ecgData AFE_ecgData;
@@ -18,51 +17,13 @@ extern volatile circularBuffer ecgSignalBuffer;
 
 uint8_t AFE_send(uint8_t data)
 {
-	AFE_TX_data = data;						//Store data to be sent
-
-	UCB1IE |= UCTXIE;							//Enable TX interrupts
-	__bis_SR_register(LPM0_bits | GIE);     	//Enter LPM0 mode, enabling global interrupts
+	UCB1TXBUF = data;				//Transmit byte (automatically clears UCTXIFG)
     while(UCB1STATW & UCBUSY){					//Wait for transmission
     }
 
     AFE_RX_data = UCB1RXBUF;				//Store received data if any
     return AFE_RX_data;
 }
-
-
-/*
- * AFE SPI TX interrupt service routine
- */
-#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
-#pragma vector=USCI_B1_VECTOR
-__interrupt void USCI_B1_ISR(void)
-#elif defined(__GNUC__)
-void __attribute__ ((interrupt(USCI_B1_VECTOR))) USCI_B1_ISR (void)
-#else
-#error Compiler not supported!
-#endif
-{
-	switch(__even_in_range(UCB1IV, USCI_SPI_UCTXIFG))
-	{
-		case USCI_NONE:
-			break;
-
-		//RX interrupt ==> New data incoming (buffer full)
-		case USCI_SPI_UCRXIFG:
-			break;
-
-		//TX interrupt ==> Can send new data (buffer empty)
-		case USCI_SPI_UCTXIFG:
-			UCB1TXBUF = AFE_TX_data;				//Transmit byte (automatically clears UCTXIFG)
-			UCB1IE &= ~UCTXIE;						//Disable TX interrupts
-			__bic_SR_register_on_exit(LPM0_bits);	//Exit LPM0
-			break;
-
-		default:
-			break;
-	}
-}
-
 
 /*
  * AFE DRDY interrupt service routine
@@ -76,9 +37,6 @@ void __attribute__ ((interrupt(PORT1_VECTOR))) Port_1 (void)
 #error Compiler not supported!
 #endif
 {
-    __bis_SR_register(GIE);		//Enable global interrupts ==> Allows interrupt nesting
-    							//							   Needed for AFE_send() TX routine
-
 	//Read 3 ADS1291 status bytes
 	int i;
 	for (i = 0; i < 3; i++) {
