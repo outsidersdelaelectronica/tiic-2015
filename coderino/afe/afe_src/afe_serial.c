49ab1c3bbb34ff2716da7a389f445f2419dbcb96
tomasvalno <tomasvalno@gmail.com>
2016-02-01 19:37:11 +0100

    New coderino, new life

---
+++ coderino/afe/afe_src/afe_serial.c
@@ -0,0 +1,74 @@
+/*
+ * afe_serial.c
+ *
+ *  Created on: 29/9/2015
+ *      Author: Smau
+ */
+
+#include "afe_serial.h"
+
+uint8_t afe_serial_send(uint8_t data)
+{
+	__delay_cycles(120);                    //Wait 7.5us (assuming 24MHz MCLK), required to allow t_sdecode to be 4 T_clk
+
+	UCB1RXBUF;								//Dummy read to empty RX buffer
+											//and clear any overrun conditions
+
+	while (!(UCB1IFG & UCTXIFG));           //Wait while not ready for TX
+	UCB1TXBUF = data;						//Transmit byte (automatically clears UCTXIFG)
+
+	while(UCB1STATW & UCBUSY);
+	while (!(UCB1IFG & UCRXIFG));
+
+    return UCB1RXBUF;				//Return received data if any
+}
+
+void afe_serial_write_register(uint8_t address, uint8_t value)
+{
+	__delay_cycles(96);                     //Wait 6us (assuming 24MHz MCLK), required to allow t_sdecode to be 4 T_clk
+											//needed to ensure previous byte was not sent within this time period
+
+	while (!(UCB1IFG & UCTXIFG));          //Wait while not ready for TX
+	UCB1TXBUF = address | WREG;
+	__delay_cycles(120);                    //Wait 7.5us (assuming 24MHz MCLK), required to allow t_sdecode to be 4 T_clk
+
+	while (!(UCB1IFG & UCTXIFG));           //Wait while not ready for TX
+	UCB1TXBUF = 0x00;						// Default size = 1
+	__delay_cycles(120);                   	//Wait 7.5us (assuming 24MHz MCLK), required to allow t_sdecode to be 4 T_clk
+
+	while (!(UCB1IFG & UCTXIFG));          	//Wait while not ready for TX
+	UCB1TXBUF = value;
+
+	while ((UCB1STAT & UCBUSY));            //Wait for all TX/RX to finish
+
+	UCB1RXBUF;                            	//Dummy read to empty RX buffer
+											//and clear any overrun conditions
+}
+
+// NOT USED
+uint8_t afe_serial_read_register(uint8_t address)
+{
+	__delay_cycles(96);
+
+	while (!(UCB1IFG & UCTXIFG)) ;      //Wait while not ready for TX
+	UCB1TXBUF = address | RREG;
+	__delay_cycles(120);               	//Wait 7.5us (assuming 24MHz MCLK), required to allow t_sdecode to be 4 T_clk
+										//this value was determined experimentally
+	while (!(UCB1IFG & UCTXIFG));       //Wait while not ready for TX
+	UCB1TXBUF = 0x00;					// Default size = 1
+	while ((UCB1STAT & UCBUSY));        //Wait for all TX/RX to finish
+
+	UCB1RXBUF;                          //Dummy read to empty RX buffer
+											//and clear any overrun conditions
+	UCB1IFG &= ~UCRXIFG;                //Ensure RXIFG is clear
+
+	// Clock the actual data transfer and receive the bytes
+
+	__delay_cycles(120);                //Wait 7.5us (assuming 24MHz MCLK), required to allow t_sdecode to be 4 T_clk
+
+	while (!(UCB1IFG & UCTXIFG));   	//Wait while not ready for TX
+	UCB1TXBUF = 0xff;					//Write dummy byte
+	while (!(UCB1IFG & UCRXIFG));       //Wait for RX buffer (full)
+
+	return UCB1RXBUF;
+}
---
+++ coderino/utils.h
@@ -0,0 +1,19 @@
+/*
+ * utils.h
+ *
+ *  Created on: 1/9/2015
+ *      Author: slopez
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+#include <msp430.h>
+#include <stdint.h>
+
+void 	itoa(uint16_t number, char* string);
+void	delay_ms(int ms);
+int32_t multiplication(int32_t op1, int32_t op2);
+int32_t division(int32_t op1, int32_t op2);
+
+#endif /* UTILS_H_ */
