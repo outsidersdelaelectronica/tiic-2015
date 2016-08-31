d5111fa25118d40310bfb2d239bc3968eb640177
supersmau <samuel.lopez.asuncion@gmail.com>
2016-08-31 09:46:51 +0200

    ECG lead selection tabs

--- code/v3.0 PersimmonOS/H2H/Src/state_ecg_6_lead.c
+++ code/v3.0 PersimmonOS/H2H/Src/state_ecg_6_lead.c
@@ -6,12 +6,26 @@
 #include "state_ecg.h"
 #include "state_running.h"
 
+/* State includes */
+#include "menu.h"
+
 /* State behaviour */
 void behaviour_ecg_6_lead(state_ptr state)
 {
   /* Set events to react to */
 
   /* Do state actions */
+  menu_ecg.items[3].item.area.is_active = GUI_ACTIVE;
+  menu_ecg.items[3].item.area.text_color = (color_t) COLOR_WHITE;
+  menu_ecg.items[3].item.area.bg_color = (color_t) COLOR_BLUE;
+
+  menu_ecg.items[4].item.area.is_active = GUI_ACTIVE;
+  menu_ecg.items[4].item.area.text_color = (color_t) COLOR_WHITE;
+  menu_ecg.items[4].item.area.bg_color = (color_t) COLOR_BLUE;
+  
+  menu_ecg.items[5].item.area.is_active = GUI_INACTIVE;
+  menu_ecg.items[5].item.area.text_color = (color_t) COLOR_BLUE;
+  menu_ecg.items[5].item.area.bg_color = (color_t) COLOR_WHITE;
 }
 
 /* Entry point to the state */
