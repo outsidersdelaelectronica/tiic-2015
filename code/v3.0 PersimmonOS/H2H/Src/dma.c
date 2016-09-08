8a7dce8478fd57f92d3cac10748c95028545ac51
tomasvalno <tomasvalno@gmail.com>
2016-09-08 14:16:05 +0200

    pintiplus working again

--- code/v3.0 PersimmonOS/H2H/Src/dma.c
+++ code/v3.0 PersimmonOS/H2H/Src/dma.c
@@ -64,8 +64,8 @@ void MX_DMA_Init(void)
   hdma_memtomem_dma2_channel1.Init.Direction = DMA_MEMORY_TO_MEMORY;
   hdma_memtomem_dma2_channel1.Init.PeriphInc = DMA_PINC_DISABLE;
   hdma_memtomem_dma2_channel1.Init.MemInc = DMA_MINC_DISABLE;
-  hdma_memtomem_dma2_channel1.Init.PeriphDataAlignment = DMA_PDATAALIGN_BYTE;
-  hdma_memtomem_dma2_channel1.Init.MemDataAlignment = DMA_PDATAALIGN_BYTE;
+  hdma_memtomem_dma2_channel1.Init.PeriphDataAlignment = DMA_PDATAALIGN_HALFWORD;
+  hdma_memtomem_dma2_channel1.Init.MemDataAlignment = DMA_MDATAALIGN_HALFWORD;
   hdma_memtomem_dma2_channel1.Init.Mode = DMA_NORMAL;
   hdma_memtomem_dma2_channel1.Init.Priority = DMA_PRIORITY_LOW;
   if (HAL_DMA_Init(&hdma_memtomem_dma2_channel1) != HAL_OK)
