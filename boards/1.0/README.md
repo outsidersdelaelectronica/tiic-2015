4278cfa4fdb8b70a4eef945be112d29965dddcec
tomasvalno <tomasvalno@gmail.com>
2017-02-07 15:36:09 +0100

    Folder deleted until cleansed to avoid massive import time.

--- boards/1.0/README.md
+++
@@ -1,2 +0,0 @@
-# tiic-2015
-Texas Instruments Innovation Challenge 2015 - Biometric authentication for pacemaker comm interfaces
--- code/dep/v3.0 PersimmonOS/H2H/Inc/tasks_bt.h
+++
@@ -1,8 +0,0 @@
-#include "FreeRTOS.h"
-#include "task.h"
-#include "cmsis_os.h"
-
-#include "bluetooth_internal.h"
-
-void tasks_bt_init();
-void tasks_bt_start();
